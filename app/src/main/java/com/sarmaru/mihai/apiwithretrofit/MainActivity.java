7a3d8c4ad07f0a4f6c553487437686dee0186219
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-06 16:22:45 +0300

    Setup RecyclerView with PostAdapter and ViewModel in MainActivity

--- app/src/main/java/com/sarmaru/mihai/apiwithretrofit/MainActivity.java
+++ app/src/main/java/com/sarmaru/mihai/apiwithretrofit/MainActivity.java
@@ -1,14 +1,43 @@
 package com.sarmaru.mihai.apiwithretrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.sarmaru.mihai.apiwithretrofit.adapters.PostAdapter;
+import com.sarmaru.mihai.apiwithretrofit.models.Post;
+import com.sarmaru.mihai.apiwithretrofit.viewmodels.PostViewModel;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PostViewModel postViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set RecyclerView and PostAdapter
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final PostAdapter postAdapter = new PostAdapter();
+        recyclerView.setAdapter(postAdapter);
+
+        // Connect ViewModel to Activity
+        postViewModel = new ViewModelProvider(this).get(PostViewModel.class);
+        postViewModel.getAllPosts().observe(this, new Observer<List<Post>>() {
+            @Override
+            public void onChanged(List<Post> posts) {
+                postAdapter.setPostsList(posts);
+            }
+        });
     }
 }
