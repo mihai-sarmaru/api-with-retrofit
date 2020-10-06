2bf4144d548fa2d5e905398e8dc1b752c85ae304
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-06 15:56:11 +0300

    Add getAllPosts method in PostRepository

--- app/src/main/java/com/sarmaru/mihai/apiwithretrofit/models/PostRepository.java
+++ app/src/main/java/com/sarmaru/mihai/apiwithretrofit/models/PostRepository.java
@@ -1,7 +1,15 @@
 package com.sarmaru.mihai.apiwithretrofit.models;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 import com.sarmaru.mihai.apiwithretrofit.apis.JsonPlaceholderApi;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -22,5 +30,26 @@ public class PostRepository {
 
     public static String getBaseUrl() {
         return BASE_URL;
+    }
+
+    public LiveData<List<Post>> getAllPosts() {
+        final MutableLiveData<List<Post>> postsList = new MutableLiveData<>();
+
+        Call<List<Post>> allPostCall = jsonPlaceholderApi.getAllPosts();
+        allPostCall.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (response.isSuccessful()) {
+                    postsList.setValue(response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                // TODO Log failure error
+            }
+        });
+
+        return postsList;
     }
 }
