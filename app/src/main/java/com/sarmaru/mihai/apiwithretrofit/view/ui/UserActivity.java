ac103f74e96fbb8460be2890ebdf0b412fe3306e
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-07 12:05:07 +0300

    Add new UserActivity and setup activity layout

---
+++ app/src/main/java/com/sarmaru/mihai/apiwithretrofit/view/ui/UserActivity.java
@@ -0,0 +1,15 @@
+package com.sarmaru.mihai.apiwithretrofit.view.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.sarmaru.mihai.apiwithretrofit.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
